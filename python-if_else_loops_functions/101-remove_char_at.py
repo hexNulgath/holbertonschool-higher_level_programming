25c204226af5bfcd2ee3b595f565ff2352ed3209
hexNulgath <machadoignacio@hotmail.com>
2024-09-03 17:56:40 -0300

    test

--- python-if_else_loops_functions/101-remove_char_at.py
+++ python-if_else_loops_functions/101-remove_char_at.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+def remove_char_at(str, n):
+    i = n
+    for letter in str:
+        if i == 0:
+            continue
+        print(f"{letter}", end="")
+        i = i - 1
+    print()
