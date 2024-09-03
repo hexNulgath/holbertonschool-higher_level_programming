63ad0fa500e35603506e80d09bd4cf1fab36d112
hexNulgath <machadoignacio@hotmail.com>
2024-09-03 17:43:36 -0300

    use only one print

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-for letter in (122, 97, -1):
+for letter in (122, 96, -1):
     if letter % 2 == 0:
-        print(f"{chr(letter)}", end="")
-    else:
-        print(f"{chr(letter - 32)}")
+        letter = letter - 32    
+    print("{}".format(chr(letter)), end="")
