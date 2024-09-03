1ffc60a087d5ae6b2561c89531090dca7a1520e7
hexNulgath <machadoignacio@hotmail.com>
2024-09-03 17:04:46 -0300

    pycodestyle

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -2,7 +2,7 @@
 def uppercase(str):
     for letter in str:
         if ord(letter) >= ord('a') and ord(letter) <= ord('z'):
-            upper= chr(ord(letter) - 32)
+            upper = chr(ord(letter) - 32)
         else:
             upper = letter
         print("{}".format(upper), end="")
