50a844f5dc8ca19388aaf9757e6ad5d5a4c28187
hexNulgath <machadoignacio@hotmail.com>
2024-09-03 16:08:10 -0300

    testing

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-digit = abs(number) % 10
+digit = number % 10
 if digit == 0:
     print(f"Last digit of {number} is {digit} and is 0")
 elif digit != 0 and digit < 6:
