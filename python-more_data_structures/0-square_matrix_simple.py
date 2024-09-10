4456c7c8501a445fc922ad44a9c58df159efe092
hexNulgath <machadoignacio@hotmail.com>
2024-09-10 16:13:40 -0300

    pycode

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -3,5 +3,5 @@ def square_matrix_simple(matrix=[]):
     new_matrix = [row[:] for row in matrix]
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
-            new_matrix[i][j] =  matrix[i][j] *  matrix[i][j]
+            new_matrix[i][j] = matrix[i][j] * matrix[i][j]
     return new_matrix
