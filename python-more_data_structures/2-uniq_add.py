40c72583899857ad8238b132dd090d86c9ef43c6
hexNulgath <machadoignacio@hotmail.com>
2024-09-10 16:46:46 -0300

    testing

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-
+    new_list = []
+    result = 0
+    for i in range(len(my_list)):
+        if my_list[i] not in new_list:
+            new_list.append(my_list[i])
+    for j in range(len(new_list)):
+        result += new_list[j]
+    return result
--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-
+    d = sorted(a_dictionary)
+    for key in d:
+        print(f"{key}: {a_dictionary[key]}")
