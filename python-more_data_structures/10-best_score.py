05398dd30a80bad01d60e06d0c1ad51fb6dd612b
hexNulgath <machadoignacio@hotmail.com>
2024-09-11 11:34:33 -0300

    testing

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-
+    score = None
+    if a_dictionary:
+        score = 0
+        for key in a_dictionary:
+            if a_dictionary[key] > score:
+                score = a_dictionary[key]
+    return score
