8d0f6b043009e746bc9238dd2d7aec656be3a09a
hexNulgath <machadoignacio@hotmail.com>
2024-09-11 11:36:25 -0300

    return names

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    score = None
+    name = None
     if a_dictionary:
         score = 0
         for key in a_dictionary:
             if a_dictionary[key] > score:
                 score = a_dictionary[key]
-    return score
+                name = key
+    return name
