f8b080e573df6f2c6e69883c5a454acea840ec44
hexNulgath <machadoignacio@hotmail.com>
2024-09-12 14:22:27 -0300

    no google

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-
+    decimal = 0
+    roman_decimal = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    if not roman_string:
+        return 0
+    for i in range(len(roman_string) - 1):
+        if roman_decimal[roman_string[i]] >= roman_decimal[roman_string[i + 1]]:
+            decimal += roman_decimal[roman_string[i]]
+        else:
+            decimal -= roman_decimal[roman_string[i]]
+    decimal += roman_decimal[roman_string[-1]]
+    return decimal
