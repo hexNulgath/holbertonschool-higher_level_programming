e1c233b9411de53ef3dde3939ecc0217b4a8a36a
hexNulgath <machadoignacio@hotmail.com>
2024-09-12 13:45:13 -0300

    test

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        result = None
+    finally: 
+        if result == None:
+            print("Inside result: None")
+        else:
+            print("Inside result: {:.2}".format(result))
+        return result
