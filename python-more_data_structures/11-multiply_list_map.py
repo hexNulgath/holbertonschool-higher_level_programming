2efbe4480a1de95a170aa02e2118901371391931
hexNulgath <machadoignacio@hotmail.com>
2024-09-12 11:56:43 -0300

    line lenght

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
-    squares = my_list[:]
-    squares = list(map(lambda x: x * number, my_list))
-    return squares
+    return list(map(lambda x: x * number, my_list))
