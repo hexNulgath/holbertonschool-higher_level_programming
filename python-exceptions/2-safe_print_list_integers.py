b3ea165f04aeb4be2ddf4fa098a8f449193a47b1
hexNulgath <machadoignacio@hotmail.com>
2024-09-13 15:16:26 -0300

    style

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -10,5 +10,4 @@ def safe_print_list_integers(my_list=[], x=0):
         except TypeError:
             continue
     print("")
-    return(count)
-
+    return count
--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -4,8 +4,8 @@ def safe_print_division(a, b):
         result = a / b
     except ZeroDivisionError:
         result = None
-    finally: 
-        if result == None:
+    finally:
+        if result is None:
             print("Inside result: None")
         else:
             print("Inside result: {:.2}".format(result))
