258c62b33659f584f9c361618600dc155734c985
hexNulgath <machadoignacio@hotmail.com>
2024-09-13 16:51:06 -0300

    all tests ready

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,1 +1,30 @@
 #!/usr/bin/python3
+""""
+     Prints a square of '#' characters with the given size.
+"""
+
+
+def print_square(size):
+
+    """
+    Parameters:
+        size (int): The size of the square to print.
+
+    Raises:
+        TypeError: If size is not an integer.
+        ValueError: If size is less than 0.
+
+    Example:
+        >>> print_square(3)
+        ###
+        ###
+        ###
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for row in range(size):
+        print("#" * size)
