ac6858fd175fe3a33f77b0c4d88019c9b033534a
hexNulgath <machadoignacio@hotmail.com>
2024-09-13 15:11:23 -0300

    readme

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,1 +1,25 @@
 #!/usr/bin/python3
+def matrix_divided(matrix, div):
+
+    if not isinstance(div,(int, float)):
+        raise TypeError("div must be a number")
+    
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    
+    if not isinstance(matrix, list):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    
+    row_size = len(matrix[0])
+    for row in matrix:
+        if len(row) != row_size:
+            raise TypeError("Each row of the matrix must have the same size")
+        
+        for element in row:
+            if not isinstance(element, (int, float)):
+                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    
+    new_matrix = [[round(element / div, 2) for element in row] for row in matrix]
+
+    return new_matrix
+ 
