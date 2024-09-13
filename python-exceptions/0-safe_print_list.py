058968766af8e7036ffc25a52e8d8c41a94a277d
hexNulgath <machadoignacio@hotmail.com>
2024-09-13 11:47:04 -0300

    changed format of print

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -3,11 +3,11 @@ def safe_print_list(my_list=[], x=0):
     count = 0
     try:
         for i in range(x):
-            print(f"{my_list[i]}", end="")
+            print(my_list[i], end="")
             count += 1
     except IndexError:
             pass
     finally:
-        print("")
+        print()
         return(count)
 
