6113c87c01e788c4b992c0137d62ba7b32dfe846
hexNulgath <machadoignacio@hotmail.com>
2024-09-13 12:54:10 -0300

    added structure

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+This module provides a function 'add_integer' that adds two integers.
+
+Args:
+    a: The first number, must be an integer or a value convertible to an integer.
+    b: The second number, must be an integer or a value convertible to an integer (default is 98).
+"""
+
+def add_integer(a, b=98):
+    """
+    Adds two integers.
+
+    Args:
+        a: The first number, must be an integer or a value convertible to an integer.
+        b: The second number, must be an integer or a value convertible to an integer (default is 98).
+
+    Returns:
+        int: The sum of a and b.
+
+    Raises:
+        TypeError: If a or b are not integers or cannot be cast to integers.
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+
+    return int(a) + int(b)
