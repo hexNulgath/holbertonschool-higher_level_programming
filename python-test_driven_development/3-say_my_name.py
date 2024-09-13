1245bf72cdebd53d01ad3276d1554ad4c895942f
hexNulgath <machadoignacio@hotmail.com>
2024-09-13 16:28:41 -0300

    all test passed

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,1 +1,31 @@
 #!/usr/bin/python3
+"""
+    Prints a greeting with the provided first and last names.
+"""
+
+
+def say_my_name(first_name, last_name=""):
+
+    """
+    Prints a greeting with the provided first and last names.
+
+    Parameters:
+        first_name (str): The first name to include in the greeting.
+        last_name (str, optional): The last name to include in
+        the greeting. Defaults to an empty string.
+
+    Raises:
+        TypeError: If `first_name` or `last_name` is not a string.
+
+    Example:
+        >>> say_my_name("John", "Doe")
+        My name is John Doe
+
+        >>> say_my_name("Jane")
+        My name is Jane
+    """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    print(f"My name is {first_name} {last_name}")
