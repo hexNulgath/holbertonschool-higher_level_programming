13c4ba0a96ede4e2775ab07eda1c03aa2801f733
hexNulgath <machadoignacio@hotmail.com>
2024-09-13 15:21:26 -0300

    conditions

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -2,6 +2,8 @@
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
-    except ValueError or TypeError:
+    except ValueError:
+        return False
+    except TypeError:
         return False
     return True
