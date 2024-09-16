49dffff6d9d975e93609022f72dcf0b4d543f829
hexNulgath <machadoignacio@hotmail.com>
2024-09-16 17:54:43 -0300

    style and doc

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
+"""
+Module that defines a Square class.
+
+This module provides a class `Square` to
+represent squares with additional features such as
+validating size
+"""
+
+
 class Square:
     """
     A class used to represent a Square.
@@ -6,13 +15,29 @@ class Square:
     Attributes
     ----------
     size : int
-        The size of the square, which represents the length of each side (default is 0).
+        The size of the square, which represents
+        the length of each side (default is 0).
     """
     def __init__(self, size=0):
+        """
+        Initializes the Square with size and position.
+
+        Parameters
+        ----------
+        size : int, optional
+            The size of the square (default is 0).
+
+        Raises
+        ------
+        TypeError
+            If size is not an integer.
+        ValueError
+            If size is negative.
+        """
         if isinstance(size, int):
             if size >= 0:
                 self.__size = size
             else:
                 raise ValueError("size must be >= 0")
-        else: 
+        else:
             raise TypeError("size must be an integer")
