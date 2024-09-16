5533b7ef2bbb56a7749adaecbdaedd3f72c036d5
hexNulgath <machadoignacio@hotmail.com>
2024-09-16 17:51:48 -0300

    style and doc

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
+"""
+Module that defines a Square class.
+
+This module provides a class `Square` to
+represent squares with additional features such as
+size
+"""
+
+
 class Square:
     """
     A class used to represent a Square.
@@ -6,7 +15,16 @@ class Square:
     Attributes
     ----------
     size : int
-        The size of the square, which represents the length of each side.
+        The size of the square, which
+        represents the length of each side.
     """
+
     def __init__(self, size):
+        """
+        Initializes the Square with size
+
+        Parameters
+        ----------
+        size : int
+        """
         self.__size = size
