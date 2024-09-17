73992559373b2aca57fcb7fff93d09ff8739484e
hexNulgath <machadoignacio@hotmail.com>
2024-09-17 09:51:41 -0300

    added doc

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,3 +1,38 @@
 #!/usr/bin/python3
+"""
+rectangle_module.py
+
+This module contains the definition of a Rectangle class,
+which is a placeholder
+for representing rectangles in future implementations.
+
+The Rectangle class will be expanded with attributes like width and height,
+and methods for computing properties such as area and perimeter.
+
+Example:
+    You can create a rectangle object once the class is implemented:
+
+        rect = Rectangle()
+
+Attributes:
+    None for now.
+
+Classes:
+    Rectangle: A class used to represent a Rectangle.
+"""
+
+
 class Rectangle:
+    """A class used to represent a Rectangle.
+
+    This is a placeholder class for future implementation of a Rectangle,
+    which will have attributes like width, height, and methods to calculate
+    area, perimeter, etc.
+
+    Attributes:
+    None for now.
+
+    Methods:
+    None for now.
+    """
     pass
