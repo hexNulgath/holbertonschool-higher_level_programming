fb2e66cb1c1003613f84f39c4a0639e6c5bc6379
hexNulgath <machadoignacio@hotmail.com>
2024-09-19 12:29:27 -0300

    changed order for cheker

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -78,8 +78,8 @@ class Rectangle:
         width (int): The width of the rectangle (default is 0).
         height (int): The height of the rectangle (default is 0).
         """
-        self.height = height
         self.width = width
+        self.height = height
         Rectangle.number_of_instances += 1
 
     @property
