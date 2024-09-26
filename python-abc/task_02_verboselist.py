a97493f661d51827774076d9f3eb08dd0ca53fd7
hexNulgath <machadoignacio@hotmail.com>
2024-09-26 13:30:40 -0300

    test

---
+++ python-abc/task_02_verboselist.py
@@ -0,0 +1,15 @@
+class VerboseList(list):
+    def append(self, item):
+        print(f"Added [{item}] to the list.")
+        super().append(item)
+    def extend(self, item):
+        print(f"Extended the list with [{len(item)}] items.")
+        super().extend(item)
+    def remove(self, item):
+        print(f"Removed [{item}] from the list.")
+        super().remove(item)
+    def pop(self, index=None):
+        if index is None:
+            index = -1
+        print(f"Popped [{self[index]}] from the list.")
+        super().pop(index)
