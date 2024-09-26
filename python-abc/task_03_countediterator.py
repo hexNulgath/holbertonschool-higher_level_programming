e6b22adfd52b432e240787663a880e4a7900a753
hexNulgath <machadoignacio@hotmail.com>
2024-09-26 14:09:40 -0300

    fixed counter naming

--- python-abc/task_03_countediterator.py
+++ python-abc/task_03_countediterator.py
@@ -1,11 +1,11 @@
 class CountedIterator():
     def __init__(self, some_iterable):
         self.iterator = iter(some_iterable)
-        self.counter = 0
+        self.count = 0
 
     def __next__(self):
-        self.counter += 1
+        self.count += 1
         return next(self.iterator)
 
     def get_count(self):
-        return self.counter
+        return self.count
