fd452bb0a43840a555fa64c252ccd955070d8c8d
hexNulgath <machadoignacio@hotmail.com>
2024-09-30 14:18:04 -0300

    doc placement

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-import json
 
 """
 This module provides a function to convert a Python object to a JSON string.
 """
 
 
+import json
 def to_json_string(my_obj):
     """
     Converts a Python object to a JSON-formatted string.
