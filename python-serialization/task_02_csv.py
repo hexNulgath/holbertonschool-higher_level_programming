861b8e9186bc760f9430626fade1430c7182a1be
hexNulgath <machadoignacio@hotmail.com>
2024-10-06 12:59:44 -0300

    updated

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -3,14 +3,14 @@
 import csv
 import json
 
-def convert_csv_to_json(file_csv, file_json):
+def convert_csv_to_json(file_csv):
     data = []
     try:
         with open(file_csv) as csv:
             csvread = csv.DictReader(csv)
             for row in csvread:
                     data.append(row)
-        with open(file_json, 'w') as json:
+        with open("data.json", 'w') as json:
             json.dump(data, json, indent=4)
         return True
     except:
