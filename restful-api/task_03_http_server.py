29d51c71b2697a78fb1314a2c35f9c52c5cbe1d3
hexNulgath <machadoignacio@hotmail.com>
2024-10-10 16:30:46 -0300

    added info path

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -6,7 +6,9 @@ import json
 
 PORT = 8000
 data = {"name": "John", "age": 30, "city": "New York"}
+info = {"version": "1.0", "description": "A simple API built with http.server"}
 json_data = json.dumps(data)
+json_info = json.dumps(info)
 
 class my_server(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
@@ -22,6 +24,12 @@ class my_server(http.server.BaseHTTPRequestHandler):
             self.wfile.write(bytes(json_data, "utf-8"))
         elif self.path == "/status":
             self.send_response(200)
+        elif self.path == "/info":
+            self.send_response(200)
+            self.send_header("content-type", "application/json")
+            self.end_headers()
+            self.wfile.write(bytes(json_info, "utf-8"))
+
         else:
             self.send_response(404)
             self.send_header("content-type", "text/html")
