6a413084e142e1f7255576669574bbaf1120dfc9
hexNulgath <machadoignacio@hotmail.com>
2024-10-28 12:34:12 -0300

    FIXED

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,9 +1,2 @@
 -- prints the following description of the table first_table from the database hbtn_0c_0 in your MySQL server.
-SELECT 
-    TABLE_NAME AS 'Table', 
-    CREATE_TABLE AS 'Create Table'
-FROM 
-    information_schema.TABLES
-WHERE 
-    TABLE_SCHEMA = DATABASE() 
-    AND TABLE_NAME = 'first_table';
+SHOW CREATE TABLE first_table;
