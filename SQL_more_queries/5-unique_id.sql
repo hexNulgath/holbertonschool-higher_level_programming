4fcc870796c83767e2fac94c4575b5d89aac3737
hexNulgath <machadoignacio@hotmail.com>
2024-10-30 10:20:47 -0300

    test

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -1,8 +1,12 @@
---creates the table unique_id on your MySQL server.
+-- Creates the database if it doesn't already exist
 CREATE DATABASE IF NOT EXISTS hbtn_test_db_5;
+
+-- Use the specified database
 USE hbtn_test_db_5;
+
+-- Create the unique_id table if it doesn't already exist
 CREATE TABLE IF NOT EXISTS unique_id (
-    id INT NOT NULL AUTO_INCREMENT, 
-    name VARCHAR(256) NOT NULL,      
-    PRIMARY KEY (id)               
+    id INT NOT NULL AUTO_INCREMENT,  -- id column with auto-increment
+    name VARCHAR(256) NOT NULL,      -- name column
+    PRIMARY KEY (id)                 -- Set id as primary key
 );
