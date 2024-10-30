796d17ae79bc5b768192fcaaba7d73c12427f43d
hexNulgath <machadoignacio@hotmail.com>
2024-10-30 10:15:22 -0300

    test

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -2,7 +2,7 @@
 CREATE DATABASE IF NOT EXISTS hbtn_test_db_5;
 USE hbtn_test_db_5;
 CREATE TABLE IF NOT EXISTS unique_id (
-id INT DEFAULT 1,
-name VARCHAR(256)
-PRIMARY KEY (id)
+    id INT NOT NULL AUTO_INCREMENT, 
+    name VARCHAR(256) NOT NULL,      
+    PRIMARY KEY (id)               
 );
