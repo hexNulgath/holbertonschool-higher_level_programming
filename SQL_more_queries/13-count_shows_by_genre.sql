db0275779a2e9a6e081c72fdd64b3f4fe8536bf9
hexNulgath <machadoignacio@hotmail.com>
2024-10-31 10:29:34 -0300

    fixed name

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,8 +1,7 @@
 -- lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each.
 
-SELECT genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
-FROM genres
-JOIN tv_show_genres ON genres.id = tv_show_genres.genre_id
+SELECT tv_show_genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
+FROM tv_show_genres
 JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
-GROUP BY genres.name
+GROUP BY tv_show_genres.name
 ORDER BY number_of_shows DESC;
