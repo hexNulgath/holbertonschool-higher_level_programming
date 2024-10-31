f87efedaba3411f770f8df51d1cf517b30816392
hexNulgath <machadoignacio@hotmail.com>
2024-10-31 10:39:28 -0300

    upload

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,9 +1,9 @@
 --  lists all genres of the show Dexter.
 
-SELECT tv_genres.name
+SELECT tv_shows.title
 FROM tv_shows
 JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-WHERE tv_shows.title = 'Dexter'
-ORDER BY tv_genres.name ASC;
+WHERE tv_genres.name = 'Comedy'
+ORDER BY tv_shows.title ASC;
 
