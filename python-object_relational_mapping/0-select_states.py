71f76d7e81375992a88012a4bbb4da9b680caebb
hexNulgath <machadoignacio@hotmail.com>
2024-11-01 11:40:06 -0300

    fixed doc

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -2,7 +2,21 @@
 import MySQLdb
 import sys
 
+
 def list_states(username, password, db_name):
+    """
+    Connect to a MySQL database, retrieve and print
+    all records from the 'states' table,
+    ordered by the 'id' column in ascending order.
+
+    Parameters:
+    - username (str): The MySQL username for authentication.
+    - password (str): The MySQL password for authentication.
+    - db_name (str): The name of the MySQL database to connect to.
+
+    Returns:
+    - None: Prints each state record to the console.
+    """
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
@@ -20,6 +34,7 @@ def list_states(username, password, db_name):
 
     cursor.close()
     db.close()
+
 
 if __name__ == "__main__":
     list_states(sys.argv[1], sys.argv[2], sys.argv[3])
