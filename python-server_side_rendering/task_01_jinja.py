78132517db45eab950b7156e7370207936f9f20d
hexNulgath <machadoignacio@hotmail.com>
2024-11-06 13:13:01 -0300

    created

--- python-server_side_rendering/task_01_jinja.py
+++ python-server_side_rendering/task_01_jinja.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
+import json
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
 @app.route('/about')
 def about():
     return render_template('about.html')
@@ -12,6 +14,19 @@ def about():
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
+
+@app.route('/items')
+def items():
+    try:
+        with open('items.json', 'r') as file:
+            data = json.load(file)  
+            items_list = data.get('items', []) 
+    except FileNotFoundError:
+        return "<h1>Error: items.json file not found</h1>"
+    except json.JSONDecodeError:
+        return "<h1>Error: Failed to decode JSON</h1>"
+    
+    return render_template('items.html', items=items_list)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
